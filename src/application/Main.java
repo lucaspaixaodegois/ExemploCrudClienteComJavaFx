fb8fd56c441e8d510fb5b9ba0049189d82473a4d
Lucas Paixão de Gois <lucaspaixaodegois@gmail.com>
2018-09-21 21:32:34 -0300

    chg:correção de bug

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,22 +8,25 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+//class principal da app
 public class Main extends Application {
-
-	public static void main(String[] args) {
-		launch(args);
+	// metodo principal que chama o launch "lançador"
+	public static void main(String args[]) {
+		launch(args);// "lançador"
 	}
 
+	// starta aplicacao
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
-		AnchorPane root = FXMLLoader.load(getClass().getResource("/view/CadastroCliente.fxml"));
-
+		// chama o arquivo.fxml que é a tela feita no scenebuilder
+		AnchorPane root = FXMLLoader.load(getClass().getResource("/view/cadastroCliente.fxml"));
+		// seta o tamanho da cena/janela
 		Scene scene = new Scene(root, 600, 400);
-
+		// seta o titulo da cena/janela
 		primaryStage.setTitle("CRUD de Clientes");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		primaryStage.setScene(scene);// seta cena/janela
+		primaryStage.show();// mostra a cena/janela
+
 	}
 
 }
