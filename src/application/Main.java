96c3a6e79c37f61ec257b2edfbe8e7b702702ada
Lucas Paixão de Gois <lucaspaixaodegois@gmail.com>
2018-09-21 21:15:06 -0300

    Meu crudCLiente

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,25 +8,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-//class principal da app
 public class Main extends Application {
-	// metodo principal que chama o launch "lançador"
-	public static void main(String args[]) {
-		launch(args);// "lançador"
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 
-	// starta aplicacao
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// chama o arquivo.fxml que é a tela feita no scenebuilder
-		AnchorPane root = FXMLLoader.load(getClass().getResource("/view/cadastroCliente.fxml"));
-		// seta o tamanho da cena/janela
-		Scene scene = new Scene(root, 600, 400);
-		// seta o titulo da cena/janela
-		primaryStage.setTitle("CRUD de Clientes");
-		primaryStage.setScene(scene);// seta cena/janela
-		primaryStage.show();// mostra a cena/janela
 
+		AnchorPane root = FXMLLoader.load(getClass().getResource("/view/CadastroCliente.fxml"));
+
+		Scene scene = new Scene(root, 600, 400);
+
+		primaryStage.setTitle("CRUD de Clientes");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 }
